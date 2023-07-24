e37fa5436f3db9ce42ac5f98440ee04eae4fecc8
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-07-24 14:41:50 +0100

    🩹 fix user get logout when upload a avatar

--- src/features/auth/useGetUser.ts
+++ src/features/auth/useGetUser.ts
@@ -3,17 +3,19 @@ import api from '../../utils/api/client'
 
 const TOKEN = localStorage.getItem('ACCESS_TOKEN')
 
-const getProfile = async (): Promise<User> => {
+const getUser = async (): Promise<User> => {
+  console.log(TOKEN)
   if (!TOKEN) return null
 
   const response = await api.get('/me')
+  console.log(response.data)
   return response.data
 }
 
 export const useGetUser = () => {
   return useQuery({
     queryKey: ['user'],
-    queryFn: () => getProfile(),
+    queryFn: () => getUser(),
     staleTime: Infinity,
     onSuccess: () => {
       // hide page loader
