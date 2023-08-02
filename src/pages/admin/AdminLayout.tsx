5c33f340b6e5fd92120c2573e441f54e15ddc134
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-08-03 00:10:40 +0100

    🚀 add production alert message

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import Navbar from '../../components/admin/ui/Navbar'
 import SideBar from '../../components/admin/ui/SideBar'
 import { useGetUser } from '../../features/auth/useGetUser'
+import ProductionAlert from '../../components/admin/ui/ProductionAlert'
 
 const AdminLayout = () => {
   const { data: user } = useGetUser()
@@ -10,6 +11,7 @@ const AdminLayout = () => {
 
   return (
     <div className="w-full pl-admin-sidebar">
+      <ProductionAlert />
       <SideBar />
       <Navbar />
       <main className="w-full min-h-screen p-6 bg-light">
