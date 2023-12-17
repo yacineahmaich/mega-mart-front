649365c8d619d951662f6666c4c73ba3ff442141
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-12-17 14:48:48 +0100

    fix: remove httpClient prefix

--- client/src/utils/api/index.ts
+++ client/src/utils/api/index.ts
@@ -1,10 +1,8 @@
 import axios, { AxiosError } from 'axios'
 
-export function createHttpClient(prefix: string = null) {
+export function createHttpClient() {
   const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}${
-      prefix ? '/' + prefix : ''
-    }`,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\FeedController;
 use Illuminate\Support\Facades\Route;
 
 
